3397eba52270211efeb15aab0fdfdf6fb31d924c
Sachinda Nirmal <32574245+sachinda123@users.noreply.github.com>
2024-08-04 08:57:29 +0530

    Delete react-typescript directory

--- react-typescript/backend/jest.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  setupFiles: ["<rootDir>/jest.setup.js"],
-  testEnvironment: "node",
-  testMatch: ["**/tests/*.js?(x)"],
-};
--- react-typescript/frontend/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { createStore, combineReducers } from "redux";
-import authReducer from "./auth.reducer";
-import signUpReducer from "./signup.reducer";
-
-export default combineReducers({
-  auth: authReducer,
-  signup: signUpReducer,
-});
